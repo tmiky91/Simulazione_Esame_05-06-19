8de926f38b10462da5efb3e752a289b772d4b1ac
pc-michi <pc-michi@192.168.1.109>
2019-06-06 14:04:45 +0200

    Modifiche

--- 2019-06-5-simulazione/src/it/polito/tdp/model/Distretto.java
+++ 2019-06-5-simulazione/src/it/polito/tdp/model/Distretto.java
@@ -4,54 +4,6 @@ import com.javadocmd.simplelatlng.LatLng;
 
 public class Distretto {
 	
-//	private int id;
-//	private double lat;
-//	private double lon;
-//	public Distretto(int id, double lat, double lon) {
-//		super();
-//		this.id = id;
-//		this.lat = lat;
-//		this.lon = lon;
-//	}
-//	public int getId() {
-//		return id;
-//	}
-//	public void setId(int id) {
-//		this.id = id;
-//	}
-//	public double getLat() {
-//		return lat;
-//	}
-//	public void setLat(double lat) {
-//		this.lat = lat;
-//	}
-//	public double getLon() {
-//		return lon;
-//	}
-//	public void setLon(double lon) {
-//		this.lon = lon;
-//	}
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + id;
-//		return result;
-//	}
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		Distretto other = (Distretto) obj;
-//		if (id != other.id)
-//			return false;
-//		return true;
-//	}
-	
 	private int id;
 	private LatLng latLon;
 	
